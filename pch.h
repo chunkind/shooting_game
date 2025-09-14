8edf939035ee7cfbd3b3b15146591b495270b485
chunkind <chunkind@naver.com>
2025-09-14 13:05:38 +0900

    9/14: 초기 셋팅

---
+++ pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <assert.h>
+#include <map>
+
+#pragma comment(lib, "Msimg32.lib")
+
+using std::vector;
+using std::string;
+using std::wstring;
+using std::map;
+using std::make_pair;
+
+#include "define.h"
+#include "struct.h"
+#include "func.h"
+#include "Resource.h"
